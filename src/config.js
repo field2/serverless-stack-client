65537243017d4363898bf87d0c8548185548e07b
Ben Dunkle <ben@field2.com>
2020-05-01 10:02:27 -0400

    Setting up our REact app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "eol-app"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://h3cov2c8hg.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2:6c38aded-163b-4547-bbfa-0372b660724d",
+    APP_CLIENT_ID: "2fcltpvca4vmbfajufq1ugmg4f",
+    IDENTITY_POOL_ID: "us-east-2_jMkTb1X4C"
+  }
+};
